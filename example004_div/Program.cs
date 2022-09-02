2f483550af35ce05a3abb34de49470d61cd0b27a
AlenaFPG <alena.rackitina2010@yandex.ru>
2022-09-02 23:45:28 +0400

    random

--- example004_div/Program.cs
+++ example004_div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = -29;
-double numberB = 14;
+﻿double numberA = new Random().Next(1,19); // 1 2 3 4 5 ... 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,19);
+Console.WriteLine(numberB);
 double div = numberA / numberB;
 Console.WriteLine(div);
