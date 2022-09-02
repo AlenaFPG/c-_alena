0411c946cb0f5b43dbe0b688f212da7126c47a9a
AlenaFPG <alena.rackitina2010@yandex.ru>
2022-09-03 03:17:19 +0400

    seminar1

---
+++ s1_project1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Console.WriteLine("Введите число");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int nume = Convert.ToInt32(Console.ReadLine());
+//if( num == nume * nume )
+//{
+//    Console.Write("да");
+//}
+//else
+//{
+//    Console.Write("нет");
+//}
+
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+string day = ("день");
+if(num == 1) 
+{
+    day = ("понедельник");
+}
+if(num == 2) 
+{
+    day = ("вторни");
+}
+if(num == 3) 
+{
+    day = ("среда");
+}
+
+Console.WriteLine(day);
